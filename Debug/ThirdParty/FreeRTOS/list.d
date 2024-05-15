be6e66cd178b492faaa90a6fc435371af0ccc377
TorekV <mustafatarikpamuk@gmail.com>
2024-05-15 23:27:16 +0300

    FreeRTOS added to plain STM32F411 project. Built without errors. Whole settings may not be done.

---
+++ Debug/ThirdParty/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRTOS/list.o: ../ThirdParty/FreeRTOS/list.c \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/list.h
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/list.h:
---
+++ Debug/ThirdParty/FreeRTOS/portable/MemMang/heap_4.d
@@ -0,0 +1,20 @@
+ThirdParty/FreeRTOS/portable/MemMang/heap_4.o: \
+ ../ThirdParty/FreeRTOS/portable/MemMang/heap_4.c \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/projdefs.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/portable.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/task.h \
+ C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/list.h
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/FreeRTOSConfig.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/projdefs.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/portable.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/mpu_wrappers.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/task.h:
+C:/Users/Tarik/STM32CubeIDE/workspace_RTOS/RTOS_Udemy/ThirdParty/FreeRTOS/include/list.h:
